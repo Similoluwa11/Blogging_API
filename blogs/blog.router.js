a8073790d51fbae536a02bdf0f0fe1c904d0b154
Similoluwa Akhigbe <akhigbesimiloluwa@gmail.com>
2023-10-30 02:36:14 +0100

    Adjusted the routes

--- blogs/blog.router.js
+++ blogs/blog.router.js
@@ -4,9 +4,9 @@ const blogController = require('./blog.controller');
 
 const router = express.Router();
 router.get('/published-blogs',blogController.getAllPublishedBlogs)
-router.get('/blogs/:id', blogController.getOneBlog)
+router.get('/:id', blogController.getOneBlog)
 
 router.post('/', middleware.authUser,  blogController.createBlog)
-router.put('/blogs/:id/publish', blogController.updateBlogState)
+router.put('/:id/publish', blogController.updateBlogState)
 
 module.exports = router
